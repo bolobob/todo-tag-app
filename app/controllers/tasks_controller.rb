b555cee57e3d659b6d405f77e60f2137defb4ad8
bolobob <aokishinichi@gmail.com>
2021-05-29 19:37:25 +0900

    ラベル表示の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -64,6 +64,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title)
+      params.require(:task).permit(:title, { label_ids: [] })
     end
 end
