9f9776435b63a767801cfae5fbe79b8a6776b820
bolobob <aokishinichi@gmail.com>
2021-06-06 22:26:32 +0900

    ラベルに紐づくタスクを取得できるように

--- app/graphql/types/label_type.rb
+++ app/graphql/types/label_type.rb
@@ -2,6 +2,7 @@ module Types
   class LabelType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: true
+    field :tasks, [Types::TaskType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
