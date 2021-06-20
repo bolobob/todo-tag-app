5f5fce1d7cd2bad0b37abf8d377fe16881513171
bolobob <aokishinichi@gmail.com>
2021-06-20 13:42:17 +0900

    material-uiで見た目を整えた

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,45 @@
 import React, { VFC } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
 import { createClient, Provider } from "urql";
 import Todos from "./Todos";
 import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const client = createClient({
   url: "http://localhost:3001/graphql",
 });
 
 const App: VFC = () => {
+  const classes = useStyles();
+
   return (
     <div className="App">
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Todos
+            </Typography>
+            <Button color="inherit">編集</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
       <Provider value={client}>
         <Todos />
       </Provider>
--- frontend/src/Todos.tsx
+++ frontend/src/Todos.tsx
@@ -1,4 +1,27 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import TextField from "@material-ui/core/TextField";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 import { useQuery } from "urql";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      backgroundColor: theme.palette.background.paper,
+    },
+    newTodoField: {
+      flex: "1",
+    },
+  })
+);
 
 const TodosQuery = `
   query {
@@ -15,17 +38,44 @@ type Task = {
 };
 
 export const Todos = () => {
+  const classes = useStyles();
   const [result] = useQuery({ query: TodosQuery });
 
   if (result.fetching) return <p>Loading...</p>;
   if (result.error) return <p>Oh no... {result.error.message}</p>;
 
   return (
-    <ul>
-      {result.data.tasks.map(({ id, title }: Task) => (
-        <li key={id}>{title}</li>
-      ))}
-    </ul>
+    <>
+      <List className={classes.root} disablePadding={true}>
+        {[0, 1, 2, 3].map((value) => {
+          const labelId = `checkbox-list-label-${value}`;
+
+          return (
+            <ListItem key={value} role={undefined} button>
+              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+              <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="comments">
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          );
+        })}
+        <ListItem>
+          <Box pr={2} className={classes.newTodoField}>
+            <TextField id="standard-basic" label="New Todo" fullWidth />
+          </Box>
+          <Button variant="contained" color="primary">
+            作成
+          </Button>
+        </ListItem>
+      </List>
+      <ul>
+        {result.data.tasks.map(({ id, title }: Task) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
