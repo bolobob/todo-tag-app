6a5e1accd81b1e33ec80315e9cb246669a3e4bd7
bolobob <aokishinichi@gmail.com>
2021-06-22 00:33:58 +0900

    useStateからuseReducerに変更

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { VFC, useState } from "react";
+import { VFC, useReducer } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -27,21 +27,45 @@ const client = createClient({
   url: "http://localhost:3001/graphql",
 });
 
+type State = {
+  isEdited: boolean;
+};
+
+type Action = {
+  type: "toggleEdited";
+};
+
+const initialState: State = {
+  isEdited: false,
+};
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "toggleEdited":
+      return { isEdited: !state.isEdited };
+    default:
+      return state;
+  }
+};
+
 const App: VFC = () => {
   const classes = useStyles();
-  const [isEdited, setIsEdited] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="App">
-      <TodoContext.Provider value={{ isEdited }}>
+      <TodoContext.Provider value={{ isEdited: state.isEdited }}>
         <div className={classes.root}>
           <AppBar position="static">
             <Toolbar>
               <Typography variant="h6" className={classes.title}>
                 Todos
               </Typography>
-              <Button onClick={() => setIsEdited(!isEdited)} color="inherit">
-                {isEdited ? "決定" : "編集"}
+              <Button
+                onClick={() => dispatch({ type: "toggleEdited" })}
+                color="inherit"
+              >
+                {state.isEdited ? "決定" : "編集"}
               </Button>
             </Toolbar>
           </AppBar>
